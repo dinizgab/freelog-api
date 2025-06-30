8a8f1644bdf8eb170388c8c0124276455382a695
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-06-30 10:50:30 -0300

    feat: create clients table

---
+++ migrations/00001_create_clients_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+create table if not exists clients (
+    id uuid primary key default gen_random_uuid(),
+    company_name varchar(255) not null,
+    contact_name varchar(255) not null,
+    contact_title varchar(255),
+    email varchar(255) not null,
+    phone varchar(50),
+    address text,
+    notes text,
+    is_active boolean default true,
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table if exists clients;
+-- +goose StatementEnd
