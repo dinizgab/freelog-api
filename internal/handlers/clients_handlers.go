5bb16e1b39d615713b26d2641c379a8995018d25
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-07-03 21:56:37 -0300

    feat: logging errors

--- internal/handlers/clients_handlers.go
+++ internal/handlers/clients_handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/freelog-projeto1/backend-freelog/internal/entity"
 	"github.com/freelog-projeto1/backend-freelog/internal/usecase"
 	"github.com/gin-gonic/gin"
@@ -10,12 +12,14 @@ func CreateClient(uc usecase.ClientsUsecase) func(*gin.Context) {
     return func(c *gin.Context) {
         var client entity.Client
         if err := c.ShouldBindJSON(&client); err != nil {
+            fmt.Printf("Error binding JSON: %v\n", err)
             c.JSON(400, gin.H{"error": "Invalid input data"})
             return
         }
 
         err := uc.CreateClient(c, client)
         if err != nil {
+            fmt.Printf("Error creating client: %v\n", err)
             c.JSON(500, gin.H{"error": "Failed to create client"})
             return
         }
