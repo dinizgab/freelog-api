6d20f9d4c2135329b1f24dea26472e8044f1eb47
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2025-09-19 07:48:22 -0300

    chore: add db logg

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,19 +4,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT as "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    dialect: process.env.DB_DIALECT as "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    logging: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-  }
 });
 
 export default sequelize;
